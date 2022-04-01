663c3e24ed09642d79dbf417fc18075dec3a296b
mananx22 <motorollaman27@gmail.com>
2022-04-01 06:00:38 -0400

    multi-line commenting and ternary operator:

--- ch3 scripting.sh
+++ ch3 scripting.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+<<commo
+This is also an example of multi-line comment in bash. 
+ternary operator in bash
+commandA && commandB || commandC 
+commo
+
 echo "you are using $( basename $0)"
 test -z $1 && echo "you didn't supplied any parameters" || echo "yes you supplied parameter $1"
 exit 0
