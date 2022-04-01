0d11d797269e3f8a1af273bc3e77786fe5dda6d7
mananx22 <motorollaman27@gmail.com>
2022-04-01 06:07:18 -0400

    rebased commit for ternary operator and multiline

--- ch3 scripting.sh
+++ ch3 scripting.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 <<commo
-This is also an example of multi-line comment in bash. 
+This is an example of multi-line comment in bash. 
 ternary operator in bash
 commandA && commandB || commandC 
 commo
+
+: '
+This is also an example of multi-line comment in bash. 
+'
 
 echo "you are using $( basename $0)"
 test -z $1 && echo "you didn't supplied any parameters" || echo "yes you supplied parameter $1"
