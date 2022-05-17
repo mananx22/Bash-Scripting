b7348ed80495d377c57b61f951b4194c52288d8d
mananx22 <motorollaman27@gmail.com>
2022-05-17 06:09:51 -0400

    Ch10 PrintF using for more formatting

--- Ch10/ch10_6 PrintColumsFormat.sh
+++ Ch10/ch10_6 PrintColumsFormat.sh
@@ -10,3 +10,9 @@ awk 'BEGIN { FS=":"}
 # Here %30s means 30 character spacing and expect an string. similarly %4d means 4 character spacing expecting integer.
 
 
+echo ">>>>>>>>>>>>>> XXXXX <<<<<<<<<<<<"
+# To apply Even more proper formatting
+awk 'BEGIN { FS=":" ; printf "%30s %4s %20s \n","Name","Process","PATH" } 
+{printf "%30s %4d %20s \n",$1,$3,$7}' /etc/passwd 
+
+
